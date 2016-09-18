eb9eaf25d2077583380ee5d534be3dc097af8439
Lukas Mueller <lamueller@protonmail.com>
2016-09-18 07:11:03 -0700

    Initial Commit

---
+++ CoreTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreTable
+//
+//  Created by Lukas Muller on 18/09/16.
+//  Copyright © 2016 Lukas Muller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
